cdf35c585fe06f0117b47cd8c4e2de76c77919fb
vigehi <vigehi2017@gmail.com>
2023-02-16 08:26:59 +0300

    updates

--- src/PayrollCalculatorServlet.java
+++ src/PayrollCalculatorServlet.java
@@ -17,9 +17,12 @@ public class PayrollCalculatorServlet extends HttpServlet {
 
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+
+    String payperiodValue = request.getParameter("payPeriod");
     String basicSalary = request.getParameter("basic-salary");
     String benefits = request.getParameter("benefits");
 
+    // double payperiodValue = Double.parseDouble(payperiod);
     double basicSalaryValue = Double.parseDouble(basicSalary);
     double benefitsValue = Double.parseDouble(benefits);
 
@@ -33,7 +36,12 @@ public class PayrollCalculatorServlet extends HttpServlet {
       nssfContribution = 1080;
     }
     double taxableIncomeValue = basicSalaryValue + benefitsValue - nssfContribution;
-    double personalReliefValue = 2400;
+    double personalReliefValue;
+    if (payperiodValue == "month") {
+      personalReliefValue = 2400;
+    } else {
+      personalReliefValue = 28800;
+    }
     double taxableIncomeAfterRelief = taxableIncomeValue - personalReliefValue;
     double payeValue;
     if (taxableIncomeAfterRelief <= 10000) {
@@ -43,7 +51,43 @@ public class PayrollCalculatorServlet extends HttpServlet {
     } else {
       payeValue = taxableIncomeAfterRelief * 0.3;
     }
-    double nhifContributionValue = 500;
+    double nhifContributionValue;
+    if (basicSalaryValue < 6000) {
+      nhifContributionValue = 150;
+    } else if (basicSalaryValue < 8000) {
+      nhifContributionValue = 300;
+    } else if (basicSalaryValue < 12000) {
+      nhifContributionValue = 400;
+    } else if (basicSalaryValue < 15000) {
+      nhifContributionValue = 500;
+    } else if (basicSalaryValue < 20000) {
+      nhifContributionValue = 600;
+    } else if (basicSalaryValue < 25000) {
+      nhifContributionValue = 750;
+    } else if (basicSalaryValue < 30000) {
+      nhifContributionValue = 850;
+    } else if (basicSalaryValue < 35000) {
+      nhifContributionValue = 900;
+    } else if (basicSalaryValue < 40000) {
+      nhifContributionValue = 950;
+    } else if (basicSalaryValue < 45000) {
+      nhifContributionValue = 1000;
+    } else if (basicSalaryValue < 50000) {
+      nhifContributionValue = 1100;
+    } else if (basicSalaryValue < 60000) {
+      nhifContributionValue = 1200;
+    } else if (basicSalaryValue < 70000) {
+      nhifContributionValue = 1300;
+    } else if (basicSalaryValue < 80000) {
+      nhifContributionValue = 1400;
+    } else if (basicSalaryValue < 90000) {
+      nhifContributionValue = 1500;
+    } else if (basicSalaryValue < 100000) {
+      nhifContributionValue = 1600;
+    } else {
+      nhifContributionValue = 1700;
+    }
+
     double netPayValue = basicSalaryValue + benefitsValue - nssfContribution - payeValue
         - nhifContributionValue;
 
@@ -55,6 +99,7 @@ public class PayrollCalculatorServlet extends HttpServlet {
     request.setAttribute("paye", payeValue);
     request.setAttribute("nhif-contribution", nhifContributionValue);
     request.setAttribute("net-pay", netPayValue);
+    request.setAttribute("payPeriod", payperiodValue);
 
     request.getRequestDispatcher("index.jsp").forward(request, response);
     // request.getRequestDispatcher("net-pay.jsp").forward(request, response);
